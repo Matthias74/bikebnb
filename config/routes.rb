651a42fcb3718aac776d0aa5712e70b09a6f06a7
Matthias Vitte <vittematthias@gmail.com>
2016-02-16 11:28:09 +0100

    update user created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'user/show'
+
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :users, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
