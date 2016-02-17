d95def81812ef945cdf005f9382205e149408596
Matthias Vitte <vittematthias@gmail.com>
2016-02-17 12:03:57 +0100

    add usercontroller and dashborad to display users id and user booking

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:index, :edit, :update]
+  resource :dashboard, only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
