7c4df8c9f315d1f5e639a17ad37bbb2c17693bef
cyrilgruter <cyrilgruter@hotmail.com>
2016-02-18 19:15:25 +0100

    time to go to bed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   resources :users, only: [:show]
   resources :bikes do
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   end
   resources :bookings, only: [:index, :edit, :update]
   resource :dashboard, only: [:show]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
